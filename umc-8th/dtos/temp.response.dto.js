10d929a11b909bb9bb36897521eaf290a9b9648c
EthanolSwab <mmm6472@naver.com>
2023-11-30 15:15:19 +0900

    first commit

---
+++ umc-8th/dtos/temp.response.dto.js
@@ -0,0 +1,9 @@
+// temp.response.dto.js
+
+export const tempResponseDTO = (data) => {
+    return {"testString" : data};
+}
+///*** 비확실 */
+export const flagResponseDTO = (data) => {
+    return {"잘못된 요청입니다." :data};
+}
